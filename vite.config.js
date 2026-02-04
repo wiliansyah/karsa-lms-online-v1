9592be1a556f6c02f84c9bd8d1e75b2420c99ef2
wiliansyah <wiliansyahilham@gmail.com>
2026-02-04 17:26:51 +0700

    initial commit 3

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react()], // Pastikan ada tanda kurung () setelah react
 })
